27e12c22a51274bf14c5c03f39ec54b377cc8d3e
hippocampus-web3 <hippocampus.web3@proton.me>
2025-05-28 12:45:05 +0200

    Remove unused code

--- src/components/layout/NodeProfileLayout.tsx
+++ src/components/layout/NodeProfileLayout.tsx
@@ -10,6 +10,7 @@ import { motion } from 'framer-motion';
 import { ChatMessageDto, NodeListingDto } from '@hippocampus-web3/runebond-client';
 import NodeHeader from './NodeHeader';
 import NodeFooter from './NodeFooter';
+import SlotMachine from '../effects/SlotMachine';
 
 interface NodeProfileLayoutProps {
   nodes: NodeListingDto[];
@@ -34,47 +35,6 @@ interface NodeProfileLayoutProps {
   walletAddress: string | null;
 }
 
-const SlotMachine: React.FC<{ text: string }> = ({ text }) => {
-  const [displayText, setDisplayText] = useState<string[]>([]);
-
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  useEffect(() => {
-    const finalText = text.split('');
-    const slots = finalText.map(() => '');
-    setDisplayText(slots);
-
-    const intervals = finalText.map((_, index) => {
-      let iterations = 0;
-      const maxIterations = 3 + index;
-
-      return setInterval(() => {
-        setDisplayText(prev => {
-          const newText = [...prev];
-          if (iterations < maxIterations) {
-            newText[index] = characters[Math.floor(Math.random() * characters.length)];
-            iterations++;
-          } else {
-            newText[index] = finalText[index];
-            clearInterval(intervals[index]);
-          }
-          return newText;
-        });
-      }, 10 + index * 2);
-    });
-
-    return () => intervals.forEach(interval => clearInterval(interval));
-  }, [text]);
-
-  return (
-    <div className="relative">
-      <div className="text-lg md:text-xl mt-2 opacity-90 font-mono bg-black/20 px-4 py-2 rounded-lg inline-block">
-        <span className="text-white tracking-wider">{displayText.join('')}</span>
-      </div>
-    </div>
-  );
-};
-
 const NodeProfileLayout: React.FC<NodeProfileLayoutProps> = (props) => {
   const { nodeAddress } = useParams<{ nodeAddress: string }>();
   const [init, setInit] = useState(false);
@@ -87,13 +47,9 @@ const NodeProfileLayout: React.FC<NodeProfileLayoutProps> = (props) => {
   };
 
   useEffect(() => {
-    console.log("Iniciando el motor de partículas...");
     initParticlesEngine(async (engine) => {
-      console.log("Cargando motor slim...");
       await loadAll(engine);
-      console.log("Motor slim cargado");
     }).then(() => {
-      console.log("Motor inicializado correctamente");
       setInit(true);
     });
   }, []);
